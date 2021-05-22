e323fbe2c08268ff596a05efd324e1c1f4973e0c
Aviad Katani <aviad@oversight.co.il>
2021-05-22 15:15:23 +0300

    Fix item url href

--- src/pages/home/Section5.tsx
+++ src/pages/home/Section5.tsx
@@ -23,7 +23,7 @@ const Section5 = () => {
           linksData.map((item: any, index: number) => {
             return (
               <div key={index}>
-                <p className="medium-text">{item.title} - <a href={item.url}>{'https://' + item.url}</a></p>
+                <p className="medium-text">{item.title} - <a href={'https://' + item.url}>{item.url}</a></p>
               </div>
             )
           })}
